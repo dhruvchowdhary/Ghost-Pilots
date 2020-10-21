4d47d6ff47db788d41eb2261c5e794976e8da9f2
Vincent <64005832@ep-student.org>
2020-10-21 10:00:36 -0500

    hi

--- APBOv2/GameScene.swift
+++ APBOv2/GameScene.swift
@@ -24,7 +24,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func didMove(to view: SKView) {
         
     player.name = "apbo"
-    player.position.x = frame.minX+75
+    player.position.x = frame.minX+300
     player.zPosition = 1
     addChild(player)
 
@@ -33,11 +33,13 @@ player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.textur
        player.physicsBody?.categoryBitMask = CollisionType.player.rawValue
         player.physicsBody?.isDynamic = false
                
-        
+        let moveBottomLeft = SKAction.move(to: CGPoint(x: 100,y: 100), duration:3.0)
+           player.run(moveBottomLeft)
     }
 }
 
 func movement() {
-    
+   
+
 }
 
