f4cd918fe51eac36188a7f920e970588c8a9aa7e
Dhruv Chowdhary <90306670@ep-student.org>
2020-11-18 10:40:47 -0600

    MainMenu stuff

---
+++ APBOv2/MainMenu/MainMenu.swift
@@ -0,0 +1,55 @@
+//
+//  MainMenu.swift
+//  APBOv2
+//
+//  Created by 90306670 on 11/18/20.
+//  Copyright © 2020 Dhruv Chowdhary. All rights reserved.
+//
+
+import SpriteKit
+
+class MainMenu: SKScene {
+
+/* UI Connections */
+var buttonPlay: MSButtonNode!
+
+    override func didMove(to view: SKView) {
+        /* Setup your scene here */
+        if let particles = SKEmitterNode(fileNamed: "Starfield") {
+                particles.position = CGPoint(x: frame.midX, y: frame.midY)
+        //      particles.advanceSimulationTime(60)
+                particles.zPosition = -1
+                addChild(particles)
+        }
+        /* Set UI connections */
+        buttonPlay = self.childNode(withName: "buttonPlay") as? MSButtonNode
+        buttonPlay.selectedHandler = {
+            self.loadGame()
+        }
+    }
+    
+    func loadGame() {
+        /* 1) Grab reference to our SpriteKit view */
+        guard let skView = self.view as SKView? else {
+            print("Could not get Skview")
+            return
+        }
+
+        /* 2) Load Game scene */
+        guard let scene = GameScene(fileNamed:"GameScene") else {
+            print("Could not make GameScene, check the name is spelled correctly")
+            return
+        }
+
+        /* 3) Ensure correct aspect mode */
+        scene.scaleMode = .aspectFill
+
+        /* Show debug */
+        skView.showsPhysics = true
+        skView.showsDrawCount = true
+        skView.showsFPS = true
+
+        /* 4) Start game scene */
+        skView.presentScene(scene)
+    }
+}
