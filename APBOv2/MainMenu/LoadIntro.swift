fff18536aa77b21009d45bab060bab8f7cf12388
Dhruv Chowdhary <90306670@ep-student.org>
2021-03-12 02:36:54 -0600

    e

--- APBOv2/MainMenu/LoadIntro.swift
+++ APBOv2/MainMenu/LoadIntro.swift
@@ -13,48 +13,18 @@ class LoadIntro: SKScene {
     
     override func didMove(to view: SKView) {
         /* Setup your scene here */
-        
         Global.gameData.skView = self.view!
         
         intropilot.position = CGPoint(x: frame.midX, y: frame.midY)
-        
-        
         intropilot.size =  CGSize(width: 250.6667, height: 247.3333)
-        
         addChild(intropilot)
-        let fadeOut = SKAction.fadeOut(withDuration: 2)
+        let fadeOut = SKAction.fadeOut(withDuration: 2.5)
         intropilot.run(fadeOut)
         
-
-        
-        let wait1 = SKAction.wait(forDuration:2)
+        let wait1 = SKAction.wait(forDuration: 2.5)
         let action1 = SKAction.run {
-            loadMainMenu()
+            Global.loadScene(s: "MainMenu")
         }
         self.run(SKAction.sequence([wait1,action1]))
-    
-        func loadMainMenu() {
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            let store = UserDefaults.standard
-            store.setValue(nil, forKey: "Score")
-            if let scene = SKScene(fileNamed: "MainMenu") {
-                // Set the scale mode to scale to fit the window
-                if UIDevice.current.userInterfaceIdiom == .pad {
-                    scene.scaleMode = .aspectFit
-                } else {
-                    scene.scaleMode = .aspectFill
-                }
-                
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            view.showsFPS = false
-            view.showsNodeCount = false
-        }
-        }
-}
+    }
 }
